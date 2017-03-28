69041eb30badbfb554e6b6532a2fc474a3e5192d
bkam03 <bkam03@yahoo.com>
2017-03-27 18:55:51 -1000

    10

--- exercises.js
+++ exercises.js
@@ -86,6 +86,8 @@ Console log the new variable. */
 
 var snacks = ["peanut butter cup oreo", "birthday cake oreo", "banana split oreo", "watermelon oreo", "cookie dough oreo", "creamsicle oreo"];
 
+console.log(snacks.slice(2));
+
 /*11. Declare two variables with names of your choosing and use the triple comparison operator (===) to compare:
 a) two strings "Racecar" and "racecar"
 b) a string and a number "three" and 3
