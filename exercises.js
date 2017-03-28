2e561707aa87033d2586593b2be3c66197d54a17
bkam03 <bkam03@yahoo.com>
2017-03-27 19:04:03 -1000

    14

--- exercises.js
+++ exercises.js
@@ -140,6 +140,13 @@ Add a property named contents and set it to be an empty array
 
 console log the object*/
 
+var bigBox = {};
+bigBox.size = Math.floor(Math.random()*12);
+bigBox.color = "red";
+bigBox.contents = [];
+
+console.log(bigBox);
+
 /*15.  Declare a literal object named "colorMeBadd" with properties and values set to:
 
    iro        => [literal object]
