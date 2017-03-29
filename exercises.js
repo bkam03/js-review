27c4f54f41de07eb07745c53281dff5975f9a18c
bkam03 <bkam03@yahoo.com>
2017-03-28 14:28:46 -1000

    #23, strings cannot be modified, only replaced

--- exercises.js
+++ exercises.js
@@ -277,6 +277,13 @@ console.log(firstReverse("boolean"));
 
 /*23. Declare a function named letterCap which takes a single parameter str. This function will capitalize the first letter of the string you pass in to invoke the function. 
 */
+function letterCap(str) {
+  var newString = str.charAt(0).toUpperCase();
+  newString = newString.concat(str.substring(1));
+  console.log(newString);
+}
+
+letterCap("declare");
 
 /*24. Declare a function named longestWord which takes a single parameter str. This function will return the longest word in the string when you invoke this function.*/ 
 
